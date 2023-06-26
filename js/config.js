f406647d29b55a4f68631c24e4dab86eeceacc57
Chetan Raj <bennchetan@gmail.com>
2023-06-26 03:11:08 -0500

    New Commit to AWS Serverless Web Application

---
+++ js/config.js
@@ -0,0 +1,11 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_vZ8EKraBV', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5pejnler48b24j2josk47bb0pk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://vygvigtnrh.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
+
